2a8d08850ad7d858d39d47c929605cd4e8f70b6a
joshuaebowling <joshuaebowling@gmail.com>
2021-09-17 23:55:56 -0400

    JBOT-34 setup libs and user spec

--- src/User.spec.ts
+++ src/User.spec.ts
@@ -1,13 +1,50 @@
 import request from "supertest"
-import app from "pro-web-app"
+import createApp from "pro-web-app"
 import { expect } from "chai"
+import Core from "pro-web-core"
+import { createPool, IResponse } from "pro-web-core"
+import { IUser } from "pro-web-core/src/interfaces/models/IUser"
+import config from "config"
+import sinon from "sinon"
+
+const userService = new Core.Service.User(config.get("db"))
+
+const stub = sinon.stub(Core.Service, "User").callsFake(function() {
+    this.verifyChallenge = function(username, challenge) {
+        return new Promise<IResponse<boolean>>((res, rej) => {
+            var resp = new Core.Response<boolean>(true)
+            if(username === "test" && challenge === "test") {
+            } else {
+                resp.Data = false
+            }
+            res(resp)    
+        })
+    }
+    this.get = function(username) {
+        return new Promise<IResponse<IUser>>((res, rej) => {
+            const resp = new Core.Response<IUser>()
+            if(username === "test") {
+                resp.Data = {
+                    username: "test",
+                    id: 1,
+                    publicKey: "test",
+                    isAdmin: false
+                }
+            } else {
+                resp.Message = Core.Enums.ResponseMessages.NotFound.toString()
+            }
+            res(resp)
+        })
+    }
+})
 
 describe("User api tests", function() {
+    const app = createApp(Core)
     describe("User create api tests", function() {
         it("post /user should return 200 + proper response", () => {
             const expectedValue: number = 1
             request(app)
-                .post("/user", {username: "test", publicKey: "test"})
+                .post("/user", {username: "[[TEST]]", publicKey: "test"})
                 .expect("Content-Type", /json/)
                 .expect(200)
                 .end((err, res) => {
@@ -32,6 +69,28 @@ describe("User api tests", function() {
                     expect(res.body).to.have.property("Message")
                     expect(res.body).to.have.property("Data")
                     expect(res.body.Data).to.equal(true)
+                })
+        })
+    })
+    describe("login tests", function() {
+        it("should not login an invalid user", async() => {
+            request(app)
+                .post(`/login`).send({username: "test", password: "abd"})
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => {
+                    expect(res.body).to.have.property("token")
+                    expect(res.body.token).to.equal(null)
+                })
+        })
+        it("should login an valid user", async() => {
+            request(app)
+                .post(`/login`).send({username: "test", password: "test"})
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => {
+                    expect(res.body).to.have.property("token")
+                    expect(res.body.token).to.be.a("string")
                 })
         })
     })
